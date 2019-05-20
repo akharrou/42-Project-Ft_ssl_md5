06da16b1ebd5bf693631cc829e097ae00e4d9cef
Aymen Kh <idev.aymen@gmail.com>
2019-05-20 13:27:56 -0700

    Latest Update

---
+++ Libft/Includes/libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/17 16:20:29 by akharrou          #+#    #+#             */
+/*   Updated: 2019/05/20 12:05:06 by akharrou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include "stdio_42.h"
+# include "stdlib_42.h"
+# include "string_42.h"
+# include "ctype_42.h"
+# include "stdint_42.h"
+# include "macros_42.h"
+# include "math_42.h"
+
+# include "bigint.h"
+
+# include <errno.h>
+
+#endif
